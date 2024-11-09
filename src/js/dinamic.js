67beee806a306b0f5887ef53d0909a8a57635f50
maviifreitass <mariavitoria.freitas@yahoo.com>
2024-11-09 19:11:31 -0300

    feat: modal para exibição dos dados e requisição

--- src/js/dinamic.js
+++ src/js/dinamic.js
@@ -2,32 +2,33 @@ import { callAPI } from './api.js';
 
 export function criarDivs(lista) {
     const container = document.getElementById('showAmostras');
-    container.innerHTML = ''; 
+    container.innerHTML = '';
+    let responseModals = new Map();
 
     for (const chave of lista.keys()) {
-        const div = document.createElement('div'); 
+        const div = document.createElement('div');
 
         div.innerHTML = `
             <h3>Ref. ${chave}</h3>
             <div class="row g-2" style="margin-bottom: 40px;">
                 <div style="display: inline">    
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                        <label class="form-check-label" for="inlineRadio1">Gramíneas</label>
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions_${chave}" id="inlineRadio1_${chave}" value="option1">
+                        <label class="form-check-label" for="inlineRadio1_${chave}">Gramíneas</label>
                     </div>
                     <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                        <label class="form-check-label" for="inlineRadio2">Macieiras</label>
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions_${chave}" id="inlineRadio2_${chave}" value="option2">
+                        <label class="form-check-label" for="inlineRadio2_${chave}">Macieiras</label>
                     </div>
                 </div>
                 <div class="col-md" style="display: inline-flex; align-items: center;">
                     <div class="form-floating" style="width: 50%; display:flex">
-                        <input type="email" class="form-control" id="floatingInputGrid" placeholder="Área plantada em Hectares" value="">
-                        <label for="floatingInputGrid">Área plantada em Hectares</label>
+                        <input type="number" class="form-control" id="floatingInputGrid_${chave}" placeholder="Área plantada em Hectares" value="">
+                        <label for="floatingInputGrid_${chave}">Área plantada em Hectares</label>
                     </div>
                     <div style="margin-left: 10px">
                         <button type="button" class="btn btn-success">Calcular</button>
-                        <button type="button" class="btn btn-primary" disabled>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detalhesModal">
                             <i class="fa fa-file-excel"></i>
                         </button>
                     </div>
@@ -37,9 +38,66 @@ export function criarDivs(lista) {
         `;
 
         const successButton = div.querySelector('.btn-success');
-        successButton.addEventListener('click', function () {
-            alert(`Detalhes da ${chave}`);
+        successButton.addEventListener('click', async function () {
+            let path = "macieiras/pre-plantio/";
+            const selectedRadio = div.querySelector(`input[name="inlineRadioOptions_${chave}"]:checked`);
+            const tipoPlanta = selectedRadio ? selectedRadio.value : null;
+
+            if (!tipoPlanta) {
+                alert("Por favor, selecione um tipo de planta.");
+                return;
+            } else if (selectedRadio.value === "option1") {
+                path = "gramineas-leguminosas-frias/pre-plantio/";
+            }
+
+            console.log(selectedRadio.value);
+
+            // Referência ao campo de input dentro do div
+            const areaPlantadaInput = div.querySelector(`#floatingInputGrid_${chave}`);
+
+            // Obtenha o valor inserido no campo de input
+            const areaPlantada = areaPlantadaInput.value;
+
+            if (!areaPlantada) {
+                alert("Por favor, preencha a área plantada em hectares.");
+                return;
+            }
+
+            // Atualizando o objeto no Map
+            const item = lista.get(chave);
+            item.areaPlantada = areaPlantada;
+
+            const response = await callAPI("POST", path, item);
+            responseModals.set(chave, response);
+            console.log("Response:", response);
+            alert("Cálculo Realizado");
         });
+
+        // Adiciona o evento ao ícone de planilha (botão Excel)
+        const excelButton = div.querySelector('.btn-primary');
+        excelButton.addEventListener('click', function () {
+            // Pega o valor do Map para o item correspondente à chave
+            const responseData = responseModals.get(chave);
+            // Preenche o conteúdo do modal
+            const modalContent = document.getElementById('modal-content');
+            modalContent.innerHTML = `
+            <h3>${responseData.message}</h3>
+            <br>
+            <h4>Dados Calculados:</h4>
+            <ul>
+                <li><strong>K Quant Hectare:</strong> ${responseData.dados.k_quant_hec.toFixed(2)}</li>
+                <li><strong>K Quant Total:</strong> ${responseData.dados.k_quant_total.toFixed(2)}</li>
+                <li><strong>P Quant Hectare:</strong> ${responseData.dados.p_quant_hec.toFixed(2)}</li>
+                <li><strong>P Quant Total:</strong> ${responseData.dados.p_quant_total.toFixed(2)}</li>
+            </ul>
+            <h4>Calagem:</h4>
+            <ul>
+                <li><strong>Calcário Calcítico:</strong> ${responseData.dados.calagem["Calcario Calcítico"].join(', ')}</li>
+                <li><strong>Calcário Dolomítico:</strong> ${responseData.dados.calagem["Calcario Dolomitico"].join(', ')}</li>
+            </ul>
+        `;
+        });
+
 
         container.appendChild(div);
     }
